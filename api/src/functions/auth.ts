a7fedeb13d7127cf17da26abda37a2ef5b0d36f6
Aldo Martinez <aldo.martinez@my.utsa.edu>
2023-07-02 09:27:23 -0500

    updated handler

--- api/src/functions/auth.ts
+++ api/src/functions/auth.ts
@@ -109,15 +109,15 @@ export const handler = async (
     // `signUp()` function in the form of: `{ message: 'String here' }`.
 
     handler: ({ username, hashedPassword, salt, userAttributes }) => {
-      return false
-      // return db.user.create({
-      //   data: {
-      //     email: username,
-      //     hashedPassword: hashedPassword,
-      //     salt: salt,
-      //     name: userAttributes.name,
-      //   },
-      // })
+      // return false
+      return db.user.create({
+        data: {
+          email: username,
+          hashedPassword: hashedPassword,
+          salt: salt,
+          name: userAttributes.name,
+        },
+      })
     },
 
     // Include any format checks for password here. Return `true` if the
